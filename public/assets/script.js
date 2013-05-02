1138644a6cde5d7792d5921cfed115a30b0fa4f8
Joe Frambach <joe@framba.ch>
2013-05-02 12:52:43 -0400

    couchsurfing widget

--- public/assets/script.js
+++ public/assets/script.js
@@ -19,3 +19,23 @@ $(function() {
   });
 });
 
+  
+function cs(username) {
+  var success = function(html) {
+    html = html.replace(/ src=/gm,' data-src=');
+    $html = $(html);
+    console.log($html.find('h1.profile').html());
+    console.log($html.find('table.profile_header').html());
+    console.log($html.find('div#ref_sum').html());
+  };
+  $.getJSON("http://query.yahooapis.com/v1/public/yql?"+
+            "q=select%20*%20from%20html%20where%20url%3D%22"+
+            encodeURIComponent('http://www.couchsurfing.org/users/'+username)+
+            "%22&format=xml'&callback=?",
+    function(data){
+        if(data.results[0]){
+            success(data.results[0]);
+        }
+    }
+  );
+}
